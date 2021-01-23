b941242c19a1e5799b4bbdb7b2f7f0f2927fc5e0
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2021-01-23 10:14:41 +0100

    Add project files.

---
+++ Catharsium.Perudo.Ui.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catharsium.Perudo.Ui.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
